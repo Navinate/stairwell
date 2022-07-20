1bc227e8392a399bf4bf459f061c53c1814dc23d
Navinate <treycluff@gmail.com>
2022-07-20 16:25:59 -0400

    updated the about page to feature lorem ipsum

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,1 +1,25 @@
-<h1 class="text-3xl">Hello</h1>
+<h1 class="text-2xl">About the StairWELL Project</h1>
+<hr />
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+	labore et dolore magna aliqua. Purus sit amet luctus venenatis lectus magna fringilla urna
+	porttitor. Nisl suscipit adipiscing bibendum est ultricies integer quis auctor. Mattis nunc sed
+	blandit libero volutpat sed cras ornare. Amet consectetur adipiscing elit duis tristique
+	sollicitudin. Sed ullamcorper morbi tincidunt ornare massa eget egestas. Eleifend donec pretium
+	vulputate sapien nec sagittis. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat
+	orci. Accumsan sit amet nulla facilisi. Nibh tortor id aliquet lectus proin nibh nisl condimentum
+	id. In hac habitasse platea dictumst vestibulum rhoncus. Viverra suspendisse potenti nullam ac.
+	Neque volutpat ac tincidunt vitae semper quis lectus nulla. Semper feugiat nibh sed pulvinar proin
+	gravida. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Libero enim sed faucibus
+	turpis.
+</p>
+
+<style>
+	h1 {
+		padding-top: 10px;
+		padding-left: 10px;
+	}
+	p {
+		padding: 10px;
+	}
+</style>
