6b1027a56227ee5227074f3adde3419373efad8a
Navinate <treycluff@gmail.com>
2023-01-17 18:16:57 -0500

    timer adjusts when a lot data is in redis

--- server/main.js
+++ server/main.js
@@ -1,8 +1,14 @@
-const minData = 10;
+const minData = 5;
 const redisKey = "stairwell";
+//in ms
+const maxInterval = 3000;
+const minInterval = 1000;
 
 const client = require("redis").createClient(); //database
 const http = require("http").createServer(); //web server
+
+let interval = maxInterval;
+let timer;
 
 //websocket setup
 const io = require("socket.io")(http, {
@@ -23,33 +29,34 @@ async function init() {
     });
   });
 
+  //launch web server
   http.listen(3000, () => console.log("Listening on port 3000"));
+
+  //init starting interval
+  timer = setInterval(popData, maxInterval);
 }
 
 async function popData() {
-  if ((await client.LLEN(redisKey)) > 0) {
+  let dataCount = await client.LLEN(redisKey);
+  console.log("data in server: " + dataCount + " -- interval: " + interval);
+
+  if (dataCount > 0) {
     //grab data from database
     let data = JSON.parse(await client.RPOP(redisKey));
     //emit to clients
-    io.emit(
-      "server to listener",
-      data.color,
-      data.a,
-      data.b,
-      data.c,
-      data.d,
-      data.e
-    );
-    console.log("emitted data");
-    //if data in database is low, re-add old data
-    if ((await client.LLEN(redisKey)) < minData) {
-      //client.LPUSH(redisKey, JSON.stringify(data));
-    }
-  } else {
-    //console.log("data empty");
+    io.emit("server to listener", data.color, data.a, data.b, data.c, data.d, data.e);
   }
+
+  //adjust interval to match amount of data
+  if (dataCount > minData) {
+    interval = Math.max(minInterval, maxInterval - dataCount * 100);
+  } else {
+    interval = Math.min(maxInterval, interval + 100);
+  }
+  //reset timer
+  clearInterval(timer);
+  //create new timer
+  timer = setInterval(popData, interval);
 }
 
 init();
-
-setInterval(popData, 3000);
